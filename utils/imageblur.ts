a96c76a28760bdb5344767c9a874c9f45f70b011
tmmin <tmminunion@gmail.com>
2023-02-28 04:09:25 +0700

    sbjbj jjjhhh jhhhmnkk

--- utils/imageblur.ts
+++ utils/imageblur.ts
@@ -8,12 +8,12 @@ export default async function getBase64ImageUrl(
   image: ImageProps
 ): Promise<string> {
   let url = cache.get(image)
-  console.log(image.imgid)
+
   if (url) {
     return url
   }
   const response = await fetch(
-    `https://bungtemin.net/images/imgthumb/${image.imgid}/400/400`
+    `https://bungtemin.net/images/imgthumb/${image.yui}/400/400`
   )
   const buffer = await response.arrayBuffer()
   const minified = await imagemin.buffer(Buffer.from(buffer), {
